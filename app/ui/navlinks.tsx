debfeca0614aca12f5f444365c000b374856647e
Rrudger <soulo@yandex.ru>
2025-01-29 17:37:11 +0100

    update header error

--- app/ui/navlinks.tsx
+++ app/ui/navlinks.tsx
@@ -14,7 +14,7 @@ const links = [
 
 interface MyComponentProps {
   mobile: bool;
-  openMenu?(): void
+  openMenu?(arg: string): void
 }
 
 export default function NavLinks({ mobile, openMenu}: MyComponentProps ) {
